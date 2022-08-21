281c8b8fa7d812f938008cceb3f95feddf315335
guptasonu14 <sguptabudha@gmail.com>
2022-08-21 10:41:18 +0530

    first day

---
+++ hello.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+//#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"Namaste India"<<endl;
+
+    int a  = 123;
+    cout<< a<< endl;
+
+    char b ='v';
+    cout<< b <<endl;
+
+    bool bl = true;
+    cout<< bl << endl;
+
+    float f = 1.2;
+    cout<< f << endl;
+
+    double d = 1.23;
+    cout<< d<<endl;
+
+
+    int size = sizeof(d);
+    cout<<"size of a:"<< size <<endl;
+    
+
+   
+     return 0;
+}
