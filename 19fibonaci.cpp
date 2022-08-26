6380dd156a806562d7c6b9ec136f4c34dc4aeb0d
guptasonu14 <sguptabudha@gmail.com>
2022-08-26 23:13:45 +0530

    ques solved

---
+++ 19fibonaci.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 10;
+
+    int a = 0;
+    int b = 1;
+
+    cout<< a << " "<< b <<" ";
+
+for (int i = 1; i <=n; i++)
+{
+    int nextNumber = a+b;
+    cout<<nextNumber<<" ";
+     a= b;
+    b = nextNumber;
+}
+   
+    
+     return 0;
+}
