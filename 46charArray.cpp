d06b068e49541e6e0744afaae7de755d81c06f5b
guptasonu14 <sguptabudha@gmail.com>
2022-10-15 08:32:14 +0530

    check palidrome

--- 46charArray.cpp
+++ 46charArray.cpp
@@ -2,6 +2,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool checkPalindrome(char a[],int n){
+    int s=0;
+    int e = n-1;
+     while(s<=e){
+        if(a[s] != a[e]){
+            return 0;
+        }
+        else{
+            s++;
+            e--;
+        }
+     }
+     return 1;
+}
+
+void reverse(char name[],int n){
+    int start=0;
+    int end = n-1;
+
+    while(start<end){
+        swap(name[start++],name[end--]);
+    }
+}
+
 int getLength(char name[]){
     int count =0;
     for(int i=0;name[i] != '\0';i++ ){
@@ -18,7 +42,16 @@ int main(){
     cout<<"your name is ";
     cout<< name <<endl;
 
-    cout<<"Length:"<<getLength(name)<<endl;
+    int len =getLength(name);
+    
+    cout<<"Length:"<<len<<endl;
+    reverse(name,len);
+      cout<<"your name is ";
+      cout<< name <<endl;
+
+
+      cout << "Palidrme or Not:" << checkPalindrome(name,len) << endl;
+
     
      return 0;
 }
