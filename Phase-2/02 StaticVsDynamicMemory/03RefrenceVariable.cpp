1c6570a2ac90122a2f0af8bc61dd3edbe99eb3d2
guptasonu14 <sguptabudha@gmail.com>
2022-10-26 15:07:31 +0530

    dynamic allocation

---
+++ Phase-2/02 StaticVsDynamicMemory/03RefrenceVariable.cpp
@@ -0,0 +1,14 @@
+//#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+///bad practice
+int& func(int n){
+    int num =a;
+    int& ans = num =0;
+    return ans;
+}
+int main(){
+     
+     return 0;
+}
---
+++ Phase-2/02 StaticVsDynamicMemory/04RefrenceVariable.cpp
@@ -0,0 +1,13 @@
+//#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    char ch = 'q';
+    cout << sizeof(ch)<<endl;
+
+    char * c = &ch ;
+    cout << sizeof(c) << endl;
+     return 0;
+}
