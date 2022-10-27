a99c81e3c1d62a0f9e086fd4d275efae052df14d
guptasonu14 <sguptabudha@gmail.com>
2022-10-27 07:29:02 +0530

    global variable

---
+++ Phase-2/03Macros, Global Variables, Inline Functions & Default Args/macro.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+#define PI 3.14
+
+int main(){
+    int r = 5;
+    double area = PI *r*r;
+    cout << "Area is" << area << endl;
+     return 0;
+}
