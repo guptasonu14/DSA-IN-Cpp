bb1a2b28b7fc6c8242ae6a693fc43c16c16f88a8
guptasonu14 <sguptabudha@gmail.com>
2022-11-12 09:38:34 +0530

    default constructor

---
+++ Phase-2/05Oops/08Constructor/Default.cpp
@@ -0,0 +1,22 @@
+//#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class construct{
+    public:
+    int a;
+    int b;
+
+    construct(){
+        a=10;
+        b =20;
+    }
+};
+
+int main(){
+
+construct c;
+cout << "a :" << c.a << endl << "b: " << c.b <<endl;
+    
+     return 0;
+}
