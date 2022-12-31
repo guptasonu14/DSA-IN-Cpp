a3a21eadb42569db29b394a848dfdd800ccb5188
guptasonu14 <sguptabudha@gmail.com>
2022-12-31 13:48:31 +0530

    recursion example

--- Phase-2/04Recursion/01Example.cpp
+++ Phase-2/04Recursion/01Example.cpp
@@ -7,16 +7,16 @@ int factorial(int n){
 
     if(n==0)
       return 1;
-   
-   return  n*factorial(n-1);
+    
+    return  n*factorial(n-1);
 }
 
 int main(){
     int n;
     cin >> n;
-
     int ans = factorial(n);
 
     cout << ans << endl;
+    
      return 0;
 }
---
+++ Phase-2/04Recursion/02Example.cpp
@@ -0,0 +1,26 @@
+//#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int power(int n){
+    //base case
+    if(n==0)
+      return 1;
+
+      //recursive relation
+
+      int smallerProblem = power(n-1);
+      int biggerProblem = 2* smallerProblem;
+
+      return biggerProblem;
+}
+
+int main(){
+     int n;
+    cin >> n;
+    int ans = power(n);
+
+    cout << ans << endl;
+    
+     return 0;
+}
