1994884aaeb8983e811655ab5beeb23d72ed5fca
guptasonu14 <sguptabudha@gmail.com>
2023-01-16 14:43:01 +0530

    array

---
+++ MyPractice/Array/01IntroArr.cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+   //initialising an array 
+    int rai[10] = {2,3,4,5,6,7,12,13,15,18};
+    
+    //accessing an aaray
+    cout << "Value at  5th index" << rai[5] << endl;
+    //cout << "Value at  5th index" << rai[11] << endl;
+
+    char ch[5] = {'a','b','c','s','g'};
+    cout<<ch[3]<<endl;
+
+    //printing the array
+    for(int i=0;i<5;i++){
+        cout << ch[i]<<" ";
+    }
+
+     cout<< " printing DONE " << endl;
+
+
+
+
+
+     return 0;
+}
