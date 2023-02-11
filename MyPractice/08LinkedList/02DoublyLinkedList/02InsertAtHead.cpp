86c7503fb981c4891157e9d4582fbd86412381fa
guptasonu14 <sguptabudha@gmail.com>
2023-02-11 15:46:11 +0530

    insert at head in dubly linked list

---
+++ MyPractice/08LinkedList/02DoublyLinkedList/02InsertAtHead.cpp
@@ -0,0 +1,67 @@
+//#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* prev;
+    Node* next;
+
+    //constructure
+    Node(int d){
+        this -> data = d;
+        this -> prev = NULL;
+        this -> next = NULL;
+    }
+};
+
+// traversing the linked list
+void print(Node* head){
+    Node* temp = head;
+
+    while(temp != NULL){
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+//to get length
+   int getLength(Node* head){
+    int len = 0;
+    Node* temp = head;
+
+    while(temp != NULL){
+        len++;
+        temp = temp -> next;
+    }
+    return len;
+   }
+void insertAtHead(Node* &head,int d){
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head -> prev = temp;
+    head = temp;
+
+}
+   
+
+int main(){
+    Node* node1 = new Node(10);
+    Node* head = node1;
+    print(head);
+
+    cout << getLength(head) << endl;
+    
+    insertAtHead(head,12);
+    print(head);
+
+     insertAtHead(head,13);
+    print(head);
+
+     insertAtHead(head,14);
+    print(head);
+    
+    
+     return 0;
+}
