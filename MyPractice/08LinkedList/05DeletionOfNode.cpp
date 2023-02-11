24d5678a69a184837a60e16c9008f1e33f1bd971
guptasonu14 <sguptabudha@gmail.com>
2023-02-11 13:05:48 +0530

    deletion of node

--- MyPractice/08LinkedList/05DeletionOfNode.cpp
+++ MyPractice/08LinkedList/05DeletionOfNode.cpp
@@ -13,7 +13,16 @@ class Node {
         this -> data = data;
         this -> next = NULL;
     }
-
+      //destructor
+    ~Node() {
+        int value = this -> data;
+        //memory free
+        if(this->next != NULL) {
+            delete next;
+            this->next = NULL;
+        }
+        cout << " memory is free for node with data " << value << endl;
+    }
   
 
 };
