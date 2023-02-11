dbbf618b256ab534a172b1fc53c78030e24e58e3
guptasonu14 <sguptabudha@gmail.com>
2023-02-11 12:35:51 +0530

    deletion of node

---
+++ MyPractice/08LinkedList/05DeletionOfNode.cpp
@@ -0,0 +1,136 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+class Node {
+
+    public:
+    int data;
+    Node* next;
+
+    //constructor
+    Node(int data) {
+        this -> data = data;
+        this -> next = NULL;
+    }
+
+  
+
+};
+
+void insertAtHead(Node* &head, int d) {
+
+    // new node create
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head = temp;
+}
+
+void insertAtTail(Node* &tail, int d) {
+     // new node create
+    Node* temp = new Node(d);
+    tail -> next = temp;
+    tail  = temp;
+}
+
+void print(Node* &head) {
+
+    if(head == NULL) {
+        cout << "List is empty "<< endl;
+        return ;
+    }
+    Node* temp = head;
+
+    while(temp != NULL ) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    cout << endl;
+}
+void insertAtPosition(Node* &tail, Node* & head, int position, int d) {
+
+
+    //insert at Start
+    if(position == 1) {
+        insertAtHead(head, d);
+        return;
+    }
+
+    Node* temp  = head;
+    int cnt = 1;
+
+    while(cnt < position-1) {
+        temp = temp->next;
+        cnt++;
+    }
+
+    //inserting at Last Position
+    if(temp -> next == NULL) {
+        insertAtTail(tail,d);
+        return ;
+    }
+
+    //creating a node for d
+    Node* nodeToInsert = new Node(d);
+
+    nodeToInsert -> next = temp -> next;
+
+    temp -> next = nodeToInsert;
+}
+
+void deleteNode(int position, Node* & head) { 
+
+    //deleting first or start node
+    if(position == 1) {
+        Node* temp = head;
+        head = head -> next;
+        //memory free start ndoe
+        temp -> next = NULL;
+        delete temp;
+    }
+    else
+    {
+        //deleting any middle node or last node
+        Node* curr = head;
+        Node* prev = NULL;
+
+        int cnt = 1;
+        while(cnt < position) {
+            prev = curr;
+            curr = curr -> next;
+            cnt++;
+        }
+
+        prev -> next = curr -> next;
+        curr -> next  = NULL;
+        delete curr;
+
+    }
+}
+
+int main() {
+    
+    //created a new node
+    Node* node1 = new Node(10);
+    
+    
+    //head pointed to node1
+    Node* head = node1; 
+    Node* tail = node1;
+   
+
+    insertAtTail(tail, 12);
+    insertAtTail(tail, 15);
+    insertAtTail(tail, 20);
+    insertAtTail(tail, 25);
+    insertAtPosition(tail,head, 1, 22);
+    deleteNode(4, head);
+    
+    
+    print(head);
+
+   
+
+
+    return 0;
+}
