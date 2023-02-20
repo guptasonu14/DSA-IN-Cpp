0c44774f44a9821cd39bee7d00378cf12eecdc4e
guptasonu14 <sguptabudha@gmail.com>
2023-02-20 20:14:13 +0530

    intro to pointer

---
+++ MyPractice/12Pointer/01Intro.cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int num = 5;
+    cout << num << endl;
+
+    //address of operator -&
+
+    cout << "address of num is" <<&num << endl;
+
+    int *ptr = &num;
+    cout <<"Address is:" << ptr << endl;
+    cout <<"value is:" << *ptr << endl;
+
+    double d = 4.2;
+    double *p2 = &d;
+    cout <<"Address is:" << p2 << endl;
+    cout <<"value is:" << *p2 << endl;
+
+    cout << "size of integer is" <<sizeof(num) <<endl;
+    cout << "size of pointer is" <<sizeof(*ptr) <<endl;
+    cout << "size of pointer is" <<sizeof(*p2) <<endl;
+
+
+
+     return 0;
+}
