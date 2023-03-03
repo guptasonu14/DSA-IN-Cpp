527908ca46be8089fe3a19399fde8264579419e8
guptasonu14 <sguptabudha@gmail.com>
2023-03-03 09:57:41 +0530

    intro to tree

--- MyPractice/14Tree/02PreOrder.cpp
+++ MyPractice/14Tree/02PreOrder.cpp
@@ -14,6 +14,15 @@ struct Node{
         right = NULL;
     }
 };
+void preorder(struct Node* root){
+    if(root ==NULL){
+        return;
+    }
+    cout << root -> data <<" ";
+    preorder(root -> left);
+    preorder(root -> right);
+
+}
 
 int main(){
     struct Node* root = new Node(1);
@@ -29,6 +38,8 @@ int main(){
 
    root->left->left = new Node(4);
    root->left->right = new Node(5);
+   root->right->left = new Node(6);
+   root->right->right = new Node(7);
 
     /*
 
@@ -39,6 +50,9 @@ int main(){
       4   5
 
     */
+   preorder(root);
+
+
    
     return 0;
 }
