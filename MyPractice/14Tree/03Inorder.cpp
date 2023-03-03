4c1194704d32dedabc906c330fc6c929142ac19d
guptasonu14 <sguptabudha@gmail.com>
2023-03-03 10:00:40 +0530

    inorder

---
+++ MyPractice/14Tree/03Inorder.cpp
@@ -0,0 +1,59 @@
+
+
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node* left = NULL;
+    struct Node* right = NULL;
+
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+void inorder(struct Node* root){
+    if(root ==NULL){
+        return;
+    }
+    
+    inorder(root -> left);
+    cout << root -> data <<" ";
+    inorder(root -> right);
+
+}
+
+int main(){
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root -> right = new Node(3);
+    /*
+
+      1
+     / \
+    2   3
+
+    */
+
+   root->left->left = new Node(4);
+   root->left->right = new Node(5);
+   root->right->left = new Node(6);
+   root->right->right = new Node(7);
+
+    /*
+
+          1
+         / \
+        2   3
+       / \
+      4   5
+
+    */
+   inorder(root);
+
+
+   
+    return 0;
+}
